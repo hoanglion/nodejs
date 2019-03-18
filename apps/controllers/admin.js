98e2af5398997ca2a3c07ae1eb31c7dbd4560b14
dinhvanlanh96 <dinhvanlanh1996@gmail.com>
2019-03-18 20:54:40 +0700

    Văn Lành
    Delete

--- apps/controllers/admin.js
+++
@@ -1,8 +0,0 @@
-var express = require("express");
-var router = express.Router();
-
-router.get("/", function(req, res) {
-  res.json({ message: "This is Admin Page" });
-});
-
-module.exports = router;
